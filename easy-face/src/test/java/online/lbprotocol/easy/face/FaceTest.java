68df11fb7ff67d51bc1946dca13c1e4157b139b4
yichen <yichen2516@qq.com>
2021-10-09 18:27:35 +0800

    N/A

--- easy-face/src/test/java/online/lbprotocol/easy/face/FaceTest.java
+++ easy-face/src/test/java/online/lbprotocol/easy/face/FaceTest.java
@@ -1,5 +1,8 @@
 package online.lbprotocol.easy.face;
 
+import com.arcsoft.face.FaceEngine;
+import com.arcsoft.face.FaceFeature;
+import com.arcsoft.face.FaceSimilar;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,6 +18,13 @@ public class FaceTest {
     @Test
     public void test() {
         FaceUtils.prepareLibFiles();
-        FaceUtils.getFaceEngine();
+        FaceEngine faceEngine = FaceUtils.getFaceEngine();
+        FaceUtils.initImageFaceEngine(faceEngine);
+        FaceFeature faceFeature = FaceUtils.extractFaceFeature(faceEngine, "/home/yichen/下载/1.jpeg");
+        System.out.println(faceFeature);
+        FaceSimilar faceSimilar = FaceUtils.compareFaceFeature(faceEngine,
+                "/home/yichen/下载/1.jpeg",
+                "/home/yichen/下载/2.jpeg");
+        System.out.println(faceSimilar.getScore());
     }
 }
