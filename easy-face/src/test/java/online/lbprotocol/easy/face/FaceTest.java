ccedce17e987f3c28945c81db854c93cbe9f3bb4
YICHEN <yichen2516@qq.com>
2021-10-09 17:11:42 +0800

    N/A

---
+++ easy-face/src/test/java/online/lbprotocol/easy/face/FaceTest.java
@@ -0,0 +1,20 @@
+package online.lbprotocol.easy.face;
+
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author yichen for easy_project
+ * @since 2021/10/9
+ */
+@SpringBootTest
+public class FaceTest {
+
+    @SneakyThrows
+    @Test
+    public void test() {
+        FaceLibrary.prepareLibFiles();
+        FaceLibrary.getFaceEngine();
+    }
+}
